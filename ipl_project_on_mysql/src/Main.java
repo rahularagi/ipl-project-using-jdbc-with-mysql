446f52b275a981d9ab007ff063c6d17923e5aa70
Rahul Aragi <rahularagi108@gmail.com>
2023-05-12 09:17:26 +0530

    commit

--- ipl_project_on_mysql/src/Main.java
+++ ipl_project_on_mysql/src/Main.java
@@ -10,56 +10,56 @@ public class Main {
         String pass="rahul";
 
         Class.forName("com.mysql.cj.jdbc.Driver");
-        Connection con= DriverManager.getConnection(url,uname,pass);
-        Statement st= con.createStatement();
+        Connection connection= DriverManager.getConnection(url,uname,pass);
+        Statement statement= connection.createStatement();
 
-       findNumberOfMatchesPlayedPerYear(st);
-       findNumberOfMatchesWonOfAllTeamsOverAllYear(st);
-       findExtraRunsConcededPerTeam(st);
-       findTopEconomicalBowlers(st);
-        st.close();
-        con.close();
+       findNumberOfMatchesPlayedPerYear(statement);
+       findNumberOfMatchesWonOfAllTeamsOverAllYear(statement);
+       findExtraRunsConcededPerTeam(statement);
+       findTopEconomicalBowlers(statement);
+        statement.close();
+        connection.close();
     }
-    public static void findNumberOfMatchesPlayedPerYear(Statement st)throws Exception{
+    public static void findNumberOfMatchesPlayedPerYear(Statement statement)throws Exception{
         String query="select season,count(*) from matches group by season";
-        ResultSet rs= st.executeQuery(query);
+        ResultSet result= statement.executeQuery(query);
         System.out.println("Number of matches played per year in ipl");
-        while(rs.next()){
-            String name="year: "+rs.getInt(1)+", matches: "+rs.getString(2);
+        while(result.next()){
+            String name="year: "+result.getInt(1)+", matches: "+result.getString(2);
             System.out.println(name);
         }
         System.out.println();
 
     }
 
-    public static void findNumberOfMatchesWonOfAllTeamsOverAllYear(Statement st)throws Exception{
+    public static void findNumberOfMatchesWonOfAllTeamsOverAllYear(Statement statement)throws Exception{
         String query="select winner,count(*) from matches where winner!='' group by winner";
-        ResultSet rs= st.executeQuery(query);
+        ResultSet result= statement.executeQuery(query);
         System.out.println("Number of matches won of all teams over all year");
-        while(rs.next()){
-            String name="Team name: "+rs.getString(1)+", Total number of times won: "+rs.getInt(2);
+        while(result.next()){
+            String name="Team name: "+result.getString(1)+", Total number of times won: "+result.getInt(2);
             System.out.println(name);
         }
         System.out.println();
 
     }
-    public static void findExtraRunsConcededPerTeam(Statement st)throws Exception{
+    public static void findExtraRunsConcededPerTeam(Statement statement)throws Exception{
         String query="select batting_team,sum(extra_runs) from deliveries where match_id In (select id from matches where season=2016) group by batting_team";
-        ResultSet rs= st.executeQuery(query);
+        ResultSet result= statement.executeQuery(query);
         System.out.println("Extra runs conceded per team in the year 2016");
-        while(rs.next()){
-            String name="Team name: "+rs.getString(1)+", Total extra runs: "+rs.getInt(2);
+        while(result.next()){
+            String name="Team name: "+result.getString(1)+", Total extra runs: "+result.getInt(2);
             System.out.println(name);
         }
         System.out.println();
 
     }
-    public static void findTopEconomicalBowlers(Statement st)throws Exception{
+    public static void findTopEconomicalBowlers(Statement statement)throws Exception{
         String query="select bowler, sum(if(wide_runs=0 and noball_runs=0,1,0)) as balls,sum(wide_runs+noball_runs+batsman_runs) as runs,(sum(wide_runs+noball_runs+batsman_runs)/(sum(if(wide_runs=0 and noball_runs=0,1,0))/6)) as 'economy' from deliveries where match_id in (select id from matches where season=2015) group by bowler order by economy";
-        ResultSet rs= st.executeQuery(query);
+        ResultSet result= statement.executeQuery(query);
         System.out.println("For the year 2015 get the top economical bowlers");
-        while(rs.next()){
-            String name="Bowler name: "+rs.getString(1)+", Total balls: "+rs.getInt(2)+", Total runs: "+rs.getInt(3)+", Economy: "+rs.getInt(4);
+        while(result.next()){
+            String name="Bowler name: "+result.getString(1)+", Total balls: "+result.getInt(2)+", Total runs: "+result.getInt(3)+", Economy: "+result.getInt(4);
             System.out.println(name);
         }
 
